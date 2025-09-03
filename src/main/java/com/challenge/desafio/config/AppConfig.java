a4b2a174363cbeb46370084a428572c094b2b41f
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-09-02 21:56:45 -0300

    desafio de 1h

---
+++ src/main/java/com/challenge/desafio/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.challenge.desafio.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    private final ObjectMapper objectMapper;
+    String conteudoArquivo =
+
+}
